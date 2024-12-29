47b37d3dce18c7d67b0217b68a188e5c35399bad
andrey.yakir <svinkabacilka@gmail.com>
2024-12-29 12:21:24 +0200

    feat: example of updating and submit modal window

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 require('./utils/env')
 
-const { App } = require('@slack/bolt');
+const {App} = require('@slack/bolt');
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
@@ -13,9 +13,143 @@ const app = new App({
 });
 
 // Listens to incoming messages that contain "hello"
-app.message('hello', async ({ message, say }) => {
+app.message('hello', async ({message, say}) => {
   // say() sends a message to the channel where the event was triggered
   await say(`Hey there <@${message.user}>!`);
+});
+
+// Listen for a slash command invocation
+app.command('/ticket', async ({ ack, body, client, logger }) => {
+  // Acknowledge the command request
+  await ack();
+
+  try {
+    // Call views.open with the built-in client
+    const result = await client.views.open({
+      // Pass a valid trigger_id within 3 seconds of receiving it
+      trigger_id: body.trigger_id,
+      // View payload
+      view: {
+        type: 'modal',
+        // View identifier
+        callback_id: 'view_1',
+        title: {
+          type: 'plain_text',
+          text: 'Hello there!'
+        },
+        blocks: [
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: 'Welcome to a modal with _blocks_'
+            },
+            accessory: {
+              type: 'button',
+              text: {
+                type: 'plain_text',
+                text: 'Click me!'
+              },
+              action_id: 'button_abc'
+            }
+          },
+          {
+            type: 'input',
+            block_id: 'input_c',
+            label: {
+              type: 'plain_text',
+              text: 'What are your hopes and dreams?'
+            },
+            element: {
+              type: 'plain_text_input',
+              action_id: 'dreamy_input',
+              multiline: true
+            }
+          }
+        ],
+        submit: {
+          type: 'plain_text',
+          text: 'Submit'
+        }
+      }
+    });
+    logger.info(result);
+  }
+  catch (error) {
+    logger.error(error);
+  }
+});
+
+// Listen for a button invocation with action_id `button_abc` (assume it's inside of a modal)
+app.action('button_abc', async ({ ack, body, client, logger }) => {
+  // Acknowledge the button request
+  await ack();
+
+  try {
+    if (body.type !== 'block_actions' || !body.view) {
+      return;
+    }
+    // Call views.update with the built-in client
+    const result = await client.views.update({
+      // Pass the view_id
+      view_id: body.view.id,
+      // Pass the current hash to avoid race conditions
+      hash: body.view.hash,
+      // View payload with updated blocks
+      view: {
+        type: 'modal',
+        // View identifier
+        callback_id: 'view_1',
+        title: {
+          type: 'plain_text',
+          text: 'Updated modal'
+        },
+        blocks: [
+          {
+            type: 'section',
+            text: {
+              type: 'plain_text',
+              text: 'You updated the modal!'
+            }
+          },
+          {
+            type: 'image',
+            image_url: 'https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExOGJ4M3R3cnBza3U2bnBtcWVkbXJ0NDF4emJ0enpqMmo3aHE4bnJpeCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/122ieBYC3AcECI/giphy.gif',
+            alt_text: 'Yay! The modal was updated'
+          }
+        ]
+      }
+    });
+    logger.info(result);
+  }
+  catch (error) {
+    logger.error(error);
+  }
+});
+
+
+// Handle a view_submission request
+app.view('view_1', async ({ ack, body, view, client, logger }) => {
+  // Acknowledge the view_submission request
+  await ack();
+
+  // Do whatever you want with the input data - here we're saving it to a DB then sending the user a verification of their submission
+
+  // Assume there's an input block with `block_1` as the block_id and `input_a`
+  const val = view['state']['values']['input_c']['dreamy_input']['value'];
+  const user = body['user']['username'];
+
+  // Message the user
+  try {
+    await client.chat.postMessage({
+      channel: '#testing',
+      text: `<@${user}>: *${val}*`,
+    });
+  }
+  catch (error) {
+    logger.error(error);
+  }
+
 });
 
 (async () => {
